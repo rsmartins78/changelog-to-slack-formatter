00b99e2378a69c13e5bd19a57666055d33d884a1
Rafael Martins <33657518+rsmartins78@users.noreply.github.com>
2022-08-25 12:42:18 +0200

    Testing workaround for text limitation

--- index.js
+++ index.js
@@ -63,14 +63,34 @@ async function run() {
       {
         type: "divider",
       },
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: changeLogFormatted,
-        },
-      },
     ];
+
+    /* 
+      Terrible workaround to deal with Slack API limitations 
+      Every 10 lines per message block, to avoind 3k characters limitation
+    */
+    let count = 0;
+    let tempText;
+    changeLogFormattedArr.forEach((change) => {
+      if (count < 10) {
+        tempText = tempText + "\n" + change;
+        count++;
+      } else if (count == 10) {
+        tempText = tempText + "\n" + change;
+        tempTextObj = {
+          type: "section",
+          text: { type: "mrkdwn", text: tempText },
+        };
+        slackMessage.push(tempTextObj);
+        tempText = "";
+        count = 0;
+      } else {
+        tempText = "";
+        count = 0;
+      }
+    });
+    /* End of workaround */
+
     const sendMessage = await slack.chat.postMessage({
       text: "A new tag was released/deployed...",
       blocks: slackMessage,
